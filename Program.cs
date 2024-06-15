b1982eca53a76dc592038ed759a861ecf1824e39
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-16 01:21:55 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_and_Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 19;
+            double salary = 500.0;
+            string name = "Abdul Hannan";
+            Console.WriteLine($"Name:{name}\nAge:{age}\nSalary:{salary}");
+
+        }
+    }
+}
